ba6b3fde3c4f847a2963892f7aefdc246f33e16d
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-08-09 20:29:23 +0400

    Shortening method

--- fizzBuzz/fizzbuzz.js
+++ fizzBuzz/fizzbuzz.js
@@ -1,15 +1,14 @@
 module.exports = function () {
   const res = [];
   for (let index = 1; index <= 100; index++) {
-    if (index % 3 === 0 && index % 5 === 0) {
-      res.push('FizzBuzz');
-    } else if (index % 5 === 0) {
-      res.push('Buzz');
-    } else if (index % 3 === 0) {
-      res.push('Fizz');
-    } else {
-      res.push(index);
-    }
+    index % 15 === 0
+      ? res.push('FizzBuzz')
+      : index % 5 === 0
+      ? res.push('Buzz')
+      : index % 3 === 0
+      ? res.push('Fizz')
+      : res.push(index);
   }
+
   return res;
 };
