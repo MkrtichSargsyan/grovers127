5b8527b410fd26b5703c3b52306eadb35f2f12d8
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-08-09 20:19:37 +0400

    Solve FizzBuzz

--- fizzBuzz/fizzbuzz.js
+++ fizzBuzz/fizzbuzz.js
@@ -1,3 +1,15 @@
 module.exports = function () {
-  
-}
+  const res = [];
+  for (let index = 1; index <= 100; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      res.push('FizzBuzz');
+    } else if (index % 5 === 0) {
+      res.push('Buzz');
+    } else if (index % 3 === 0) {
+      res.push('Fizz');
+    } else {
+      res.push(index);
+    }
+  }
+  return res;
+};
