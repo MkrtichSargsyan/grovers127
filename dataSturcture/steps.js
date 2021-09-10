49c0f5e875c20bf386af21a8e6c1814ca3e0fbbb
rahalrazika <rahalraika@gmail.com>
2021-09-11 00:00:19 +0200

    add recursive method to the steps solution

--- dataSturcture/steps.js
+++ dataSturcture/steps.js
@@ -26,3 +26,24 @@ for (let row = 0; row < num; row++) {
 }
 }
 step(3)
+console.log('---------------------');
+// recursive solution 
+const recursive = (num,res) => {
+    let temp = ''
+    if(num === 0){
+        console.log(res);
+        return res
+    }
+
+    for (let index = num; index > 0 ; index--) {
+        temp +='#'
+    }
+
+    res = temp +'\n' + res
+ 
+
+   return recursive(num-1,res)
+
+}
+
+console.log(recursive(6,''))
